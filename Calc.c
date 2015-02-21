ad78dcaf1d64bf8508dfbf91acd012507ac6ef2e
Yashwant Keswani <yashwant.keswani@live.com>
2015-02-21 11:51:51 +0530

    Initial Commit

---
+++ Calc.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int diff(int a,int b)
+{
+	return a-b;
+}
+
+int add(int a,int b)
+{
+	return a+b;
+}
+
+int divide(int a,int b)
+{
+	return a?b;
+}
+
+int multiply(int a,int b)
+{
+	return a*b;
+}
+
+int main()
+{
+	int x, y;
+	printf("Enter two values: ");
+	scanf("%d %d", &x, &y);
+	printf("%d, %d, %d, %d", add(x, y), diff(x, y), divide(x, y), multiply(x, y));
+	return 0;
+}
